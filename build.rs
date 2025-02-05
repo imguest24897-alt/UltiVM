4c6bea1b36c1d492825555456e3b9af6292692de
imguest24897-alt <161818573+imguest24897-alt@users.noreply.github.com>
2025-02-05 12:02:31 +0200

    a litte update

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
+    println!("Started build...");
     let out_dir = env::var("OUT_DIR").unwrap();
     let source = Path::new("req/config.ini");
     let dest = Path::new(&out_dir).join("config.ini");
---
+++ src/gui.rs
@@ -0,0 +1,2 @@
+// this is unused for now, planned to be as gui for the control panel of the server in the future
+// once it will be used, you can trigger it with --gui or -g flag
